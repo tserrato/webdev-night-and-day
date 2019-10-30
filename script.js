0ab250fcbb55e710081f4548ac0423e37b3f1445
Tyler Serrato <tserrato21351@columbushs.com>
2019-10-29 20:00:01 -0400

    Done

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-$(document).ready(function() {
-    $('button').click(function(){
-        if ($('body').hasClass('black')) {
-            $('body').removeClass('black').addClass('white');
+$( document ).ready( function() {
+    $('#orb').click( function() {
+        if ($('#orb').hasClass('sun')) {
+            $('#orb').removeClass('sun').addClass('moon');
         }
         else {
-            $('body').removeClass('white').addClass('black');
+            $('#orb').removeClass('moon').addClass('sun');
+        }
+
+        if ($('body').hasClass('day')) {
+            $('body').removeClass('day').addClass('night');
+        }
+        else {
+            $('body').removeClass('night').addClass('day');
+        }
+        if ($('#moonspot1').hasClass('visible')) {
+            $('#moonspot1').removeClass('visible');
+        } else {
+            $('#moonspot1').addClass('visible');
+        }
+        if ($('#moonspot2').hasClass('visible')) {
+            $('#moonspot2').removeClass('visible');
+        } else {
+            $('#moonspot2').addClass('visible');
+        }
+        if ($('#moonspot3').hasClass('visible')) {
+            $('#moonspot3').removeClass('visible');
+        } else {
+            $('#moonspot3').addClass('visible');
         }
     });
 });
